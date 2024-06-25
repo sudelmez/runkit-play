3fe43bb730d5a812bca66cf4a9355608b35c319f
Sude Ölmez <98457237+sudelmez@users.noreply.github.com>
2024-06-25 15:15:10 +0300

    runkit url update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Embed from 'react-runkit';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [data, setData] = useState("");
+  const getData = async () => {
+    try {
+      const res = await fetch("https://hellorunkit-onog2rtymt8o.runkit.sh", {
+        method: 'GET',
+      });
+      const json = await res.json();
+      setData(json.message);
+      console.log("response:");
+      console.log(json);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    getData();
+  }, [data])
+  const helloSource = `console.log('Hello, world!')`
+  return (<div>
+    <h1>{data}</h1>
+    <Embed source={helloSource} />
+  </div>
   );
 }
 
